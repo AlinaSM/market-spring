928d13fb2f7df1cce6553863b6d477bd824f10a5
AlinaSM <alinasalmen995@gmail.com>
2022-02-08 14:32:51 -0600

    Prueba de Controlador Hola Mundo

---
+++ src/main/java/com/asm/market/AsmMarketApplication.java
@@ -0,0 +1,13 @@
+package com.asm.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asm/market/AsmMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asm.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
