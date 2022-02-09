d6989fbf74edd7851b87b023c4efe70b54d3a851
AlinaSM <alinasalmen995@gmail.com>
2022-02-08 22:11:20 -0600

    Mapeo de tablas clientes y categorias en Clases(Entity)

---
+++ src/main/java/com/asm/market/persistence/entity/Category.java
@@ -0,0 +1,43 @@
+package com.asm.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer id;
+
+    @Column(name = "descripcion")
+    private String description;
+
+    @Column(name = "estado")
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
