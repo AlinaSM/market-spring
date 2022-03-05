3e85041066e885d150471ba8caf9887727a7e1ac
AlinaSM <alinasalmen995@gmail.com>
2022-03-04 21:30:11 -0600

    Query methods e implementacion de Repository en Products

--- src/main/java/com/asm/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/asm/market/persistence/crud/ProductCrudRepository.java
@@ -1,7 +1,23 @@
 package com.asm.market.persistence.crud;
 
 import com.asm.market.persistence.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+
+    // Query nativo
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true )
+    // List<Product> getByIdCategory(int idCategory);
+
+    // Query method
+    List<Product> findByIdCategoryOrderByNameAsc(int idCategory);
+
+    Optional<List<Product>> findByAmountInStockLessThanAndStatus(int amountInStock, boolean status);
+
+
+
 }
