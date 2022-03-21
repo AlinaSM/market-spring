6930406dfe86b796abbcf35dda67e6729d105044
AlinaSM <alinasalmen995@gmail.com>
2022-03-21 17:48:07 -0600

    Definiendo las respuestas de los endopoints del controlador Product

--- src/main/java/com/asm/market/web/controller/ProductController.java
+++ src/main/java/com/asm/market/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.asm.market.web.controller;
 import com.asm.market.domain.ProductDomain;
 import com.asm.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,32 +17,43 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    public List<ProductDomain> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<ProductDomain>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public Optional<ProductDomain> getProduct( @PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<ProductDomain> getProduct( @PathVariable("id") int productId){
+        //El orElse se ejecuta cuando en el map no se ejecuta, y esto cuando ocurre?, justo cuando el optional de getProduct no obtenga resultado
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
     }
 
     @GetMapping("category/{categoryId}")
-    public Optional<List<ProductDomain>> getByCategory( @PathVariable("categoryId") int categoryId){
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<ProductDomain>> getByCategory( @PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map( products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
-    public ProductDomain save(@RequestBody ProductDomain productDomain){
-        return productService.save(productDomain);
+    public ResponseEntity save(@RequestBody ProductDomain productDomain){
+        return new ResponseEntity<>(productService.save(productDomain), HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
-    public boolean delete( @PathVariable("id") int productId){
-        return getProduct(productId).map( prod -> {
+    public ResponseEntity delete( @PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity(HttpStatus.OK);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        /*return getProduct(productId).map( prod -> {
             productService.delete(productId);
             return true;
         }).orElse( false);
-        /*
+
         if(getProduct(productId).isPresent()){
             productService.delete(productId);
             return true;
