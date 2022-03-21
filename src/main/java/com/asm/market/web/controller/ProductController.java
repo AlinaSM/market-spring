126af931b4d1ffac576d1fe6f968e51790c1d5a5
AlinaSM <alinasalmen995@gmail.com>
2022-03-21 16:37:45 -0600

    Definiendo los endopoints del controlador Product

--- src/main/java/com/asm/market/web/controller/ProductController.java
+++ src/main/java/com/asm/market/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.asm.market.web.controller;
 import com.asm.market.domain.ProductDomain;
 import com.asm.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,23 +14,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping()
     public List<ProductDomain> getAll(){
         return productService.getAll();
     }
 
-    public Optional<ProductDomain> getProduct(int productId){
+    @GetMapping("{id}")
+    public Optional<ProductDomain> getProduct( @PathVariable("id") int productId){
         return productService.getProduct(productId);
     }
 
-    public Optional<List<ProductDomain>> getByCategory(int categoryId){
+    @GetMapping("category/{categoryId}")
+    public Optional<List<ProductDomain>> getByCategory( @PathVariable("categoryId") int categoryId){
         return productService.getByCategory(categoryId);
     }
 
-    public ProductDomain save(ProductDomain productDomain){
+    @PostMapping()
+    public ProductDomain save(@RequestBody ProductDomain productDomain){
         return productService.save(productDomain);
     }
 
-    public boolean delete(int productId){
+    @DeleteMapping("{id}")
+    public boolean delete( @PathVariable("id") int productId){
         return getProduct(productId).map( prod -> {
             productService.delete(productId);
             return true;
