96f6b16fdcb0004b998227388b8f1099cfaef4e5
AlinaSM <alinasalmen995@gmail.com>
2022-08-28 15:22:45 -0500

    Implementando OpenApi3 para la documentacion de la API

--- src/main/java/com/asm/market/web/controller/ProductController.java
+++ src/main/java/com/asm/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.asm.market.web.controller;
 
 import com.asm.market.domain.ProductDomain;
 import com.asm.market.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,50 +21,57 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
+    @Operation(summary = "Get all products")
+    @ApiResponse(responseCode = "200", description = "OK")
     public ResponseEntity<List<ProductDomain>> getAll(){
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<ProductDomain> getProduct( @PathVariable("id") int productId){
+    @Operation(summary = "Get products by id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+    })
+    public ResponseEntity<ProductDomain> getProduct( @Parameter(description = "id of the product", required = true, example = "7")
+    @PathVariable("id") int productId){
         //El orElse se ejecuta cuando en el map no se ejecuta, y esto cuando ocurre?, justo cuando el optional de getProduct no obtenga resultado
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-
     }
 
     @GetMapping("category/{categoryId}")
-    public ResponseEntity<List<ProductDomain>> getByCategory( @PathVariable("categoryId") int categoryId){
+    @Operation(summary = "Get products by an id of category")
+    @ApiResponses({
+            @ApiResponse( responseCode = "200", description = "OK"),
+            @ApiResponse( responseCode = "404", description = "Products not found with the id of category given"),
+    })
+    public ResponseEntity<List<ProductDomain>> getByCategory( @Parameter(description = "Id  of the category", required = true, example = "1") @PathVariable("categoryId") int categoryId){
         return productService.getByCategory(categoryId)
                 .map( products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
+    @Operation(summary = "Save product")
+    @ApiResponse(responseCode = "201", description = "Product save")
     public ResponseEntity save(@RequestBody ProductDomain productDomain){
         return new ResponseEntity<>(productService.save(productDomain), HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity delete( @PathVariable("id") int productId){
+    @Operation(summary = "Delete a product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+    })
+    public ResponseEntity delete( @Parameter(description = "product's id") @PathVariable("id") int productId){
         if(productService.delete(productId)){
             return new ResponseEntity(HttpStatus.OK);
         }else{
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        /*return getProduct(productId).map( prod -> {
-            productService.delete(productId);
-            return true;
-        }).orElse( false);
-
-        if(getProduct(productId).isPresent()){
-            productService.delete(productId);
-            return true;
-        }else{
-            return false;
-        }
-        */
     }
 
 }
--- src/main/java/com/asm/market/web/controller/PurchaseController.java
+++ src/main/java/com/asm/market/web/controller/PurchaseController.java
@@ -2,6 +2,10 @@ package com.asm.market.web.controller;
 
 import com.asm.market.domain.PurchaseDomain;
 import com.asm.market.domain.service.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +21,27 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping()
+    @Operation(summary = "Get all the purcheses")
+    @ApiResponse( responseCode = "200", description = "OK")
     public ResponseEntity<List<PurchaseDomain>> getAll(){
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("client/{idClient}")
-    public ResponseEntity<List<PurchaseDomain>> getByClient( @PathVariable("idClient") String clientId){
+    @Operation(summary = "Get purchuses by a client")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Purchuse not found"),
+    })
+    public ResponseEntity<List<PurchaseDomain>> getByClient( @Parameter(description = "Client's id", required = true, example = "4546221") @PathVariable("idClient") String clientId){
         return purchaseService.getByClient(clientId)
                 .map(purchase ->  new ResponseEntity<>(purchase,HttpStatus.OK ))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
+    @Operation(summary = "Save purchase")
+    @ApiResponse(responseCode = "201", description = "CREATED")
     public ResponseEntity save(PurchaseDomain purchaseDomain){
         return new ResponseEntity<>(purchaseService.save(purchaseDomain), HttpStatus.CREATED);
     }
